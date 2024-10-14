3976a8c92161311b02442f8386ebd05913222f9f
ahnaf <ahnaf@ahnafs-Mac-mini.local>
2024-10-14 20:54:37 +0600

    done

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import HomeRIghtComponent from '../../Components/HomeComponent/HomeRightComponents/HomeRIghtComponent';
+import {getAuth, onAuthStateChanged} from 'firebase/auth';
+import NotVerifide from '../../Components/HomeComponent/HomeRightComponents/NotVerifide/NotVerifide';
 
 const Home = () => {
+  const auth = getAuth();
+
+  const [isEmailVerifide, setisEmailVerifide] = useState(null);
+  console.log(isEmailVerifide);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setisEmailVerifide(user.reloadUserInfo.emailVerified);
+    });
+  }, []);
   return (
-    <>
-      <HomeRIghtComponent />
-    </>
+    <div>{isEmailVerifide ? <HomeRIghtComponent /> : <NotVerifide />}</div>
   );
 };
 
